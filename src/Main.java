254d028983eada7f8c2b5aac8f7c20476839840e
OXL <oxl02@qq.com>
2022-05-01 20:40:27 +0800

    20220501

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("abcdefg");
-        System.out.println("test");
+        System.out.println("Hello!This is my Java experiment.");
     }
 }
