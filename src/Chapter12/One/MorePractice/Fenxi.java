35f6e66f4f25b7d4e72eee638654ae1d8365e1a6
OXL <oxl02@qq.com>
2022-05-25 22:14:12 +0800

    20220525

---
+++ src/Chapter12/One/MorePractice/Fenxi.java
@@ -0,0 +1,18 @@
+package Chapter12.One.MorePractice;
+
+import java.util.StringTokenizer;
+
+public class Fenxi {
+    public static double getTotalScore(String s) {
+        String regex="[^0123456789.]" ; //匹配非数字的正则表达识
+        String digitMess=s.replaceAll(regex,"*");
+        StringTokenizer fenxi = new StringTokenizer(digitMess,"*");
+        double totalScore=0;
+        while(fenxi.hasMoreTokens()){
+            double score = Double.parseDouble(fenxi.nextToken());
+            totalScore = totalScore+score;
+//            System.out.println(score);
+        }
+        return totalScore;
+    }
+}
